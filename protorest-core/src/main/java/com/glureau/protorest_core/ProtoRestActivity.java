9d6b7d7c7b2eaf6fa15b476217a407e86c151ec5
Greg <gregorylureau@gmail.com>
2017-09-29 16:18:46 +0200

    Setup library with a sample project.

---
+++ protorest-core/src/main/java/com/glureau/protorest_core/ProtoRestActivity.java
@@ -0,0 +1,7 @@
+package com.glureau.protorest_core;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ProtoRestActivity extends AppCompatActivity {
+
+}
---
+++ protorest-sample-github/src/main/java/com/glureau/protorest_sample_github/Main.kt
@@ -0,0 +1,7 @@
+package com.glureau.protorest_sample_github
+
+import android.app.Application
+import com.glureau.protorest_core.ProtoRestActivity
+
+class MainApplication : Application()
+class MainActivity : ProtoRestActivity()
