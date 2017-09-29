3f88f5d5db8a66f5c9e87dc3f84be6573c2df8c5
Greg <gregorylureau@gmail.com>
2017-09-30 01:55:14 +0200

    Prepare dynamic left menu.

--- protorest-core/src/main/java/com/glureau/protorest_core/ProtoRestActivity.kt
+++ protorest-core/src/main/java/com/glureau/protorest_core/ProtoRestActivity.kt
@@ -1,7 +1,11 @@
 package com.glureau.protorest_core
 
 import android.os.Bundle
-import io.reactivex.Observable
+import android.support.v4.view.GravityCompat
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ProtoRestActivity : DefaultFeatureActivity() {
     private lateinit var root: ProtoRestApplication<*>
@@ -9,12 +13,18 @@ class ProtoRestActivity : DefaultFeatureActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         root = application as ProtoRestApplication<*>
+        val menu = nav_view.menu
         for ((featureName, action) in root.setup) {
-            addFeatureButton(featureName, action)
+            val item = menu.add(0, featureName.hashCode(), Menu.NONE, featureName)
+            item.setIcon(R.drawable.ic_menu_manage)
+            item.setOnMenuItemClickListener {
+                Toast.makeText(this, "Hello $featureName", Toast.LENGTH_LONG).show()
+                drawer_layout.closeDrawer(GravityCompat.START)
+                true
+            }
         }
     }
 
-    private fun addFeatureButton(featureName: String, action: () -> Observable<RestResult>) {
-//        layoutInflater.inflate(R.layout.feature_button, this)
-    }
+    // Easier to use a direct click listener
+    override fun onNavigationItemSelected(item: MenuItem) = true
 }
