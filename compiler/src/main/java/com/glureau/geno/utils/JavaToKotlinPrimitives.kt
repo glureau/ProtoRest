75f897d8750035207a9f76110eebf689bcb0ef5b
Greg <gregorylureau@gmail.com>
2017-11-03 11:02:09 +0100

    Fix java Integer to kotlin Int primitive transformation.

--- compiler/src/main/java/com/glureau/geno/utils/JavaToKotlinPrimitives.kt
+++ compiler/src/main/java/com/glureau/geno/utils/JavaToKotlinPrimitives.kt
@@ -15,7 +15,7 @@ object JavaToKotlinPrimitives {
             "java.lang.Double", "java.lang.Double?" -> KotlinNullable.asTypeName(Double::class, typeName.nullable)
             "java.lang.Float", "java.lang.Float?" -> KotlinNullable.asTypeName(Float::class, typeName.nullable)
             "java.lang.Long", "java.lang.Long?" -> KotlinNullable.asTypeName(Long::class, typeName.nullable)
-            "java.lang.Int", "java.lang.Int?" -> KotlinNullable.asTypeName(Int::class, typeName.nullable)
+            "java.lang.Integer", "java.lang.Integer?" -> KotlinNullable.asTypeName(Int::class, typeName.nullable)
             "java.lang.Short", "java.lang.Short?" -> KotlinNullable.asTypeName(Short::class, typeName.nullable)
             "java.lang.Byte", "java.lang.Byte?" -> KotlinNullable.asTypeName(Byte::class, typeName.nullable)
             else -> typeName
